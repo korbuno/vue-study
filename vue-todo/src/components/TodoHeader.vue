da17ac70e8eb38e770e773c85e62b57011d479ca
korbuno <aileen528@naver.com>
2018-08-02 22:51:28 +0900

    뷰 공부

---
+++ vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        <h1>TODO it!</h1>
+    </header>
+</template>
+
+
+<!-- scoped : 컴포넌트 안에서만 적용 -->
+<style scoped>
+h1 {
+    color: #2F3B52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+}
+</style>
